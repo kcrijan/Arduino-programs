24678efe9366a8cc3dbf7334550c39277ff2f095
kcrijan <108547145+kcrijan@users.noreply.github.com>
2022-07-02 00:03:44 +0545

    Create fade.ino

---
+++ fade.ino
@@ -0,0 +1,18 @@
+int pwmPin = 12;
+
+
+void setup() {
+ pinMode(pwmPin, OUTPUT); 
+}
+
+void loop() {
+for(int value = 0; value <= 255; value++){
+  analogWrite(pwmPin, value);
+  delay(30);
+}
+for(int value = 255;value >= 0;value--){
+  analogWrite(pwmPin,value);
+  delay(20);
+}
+
+}
